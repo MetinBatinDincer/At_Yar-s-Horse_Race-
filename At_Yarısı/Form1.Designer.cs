a7c24920d6b213db5c1584571c78c4a9fa852879
Batın <Batn@DESKTOP-EBT7U3B>
2023-01-30 21:58:01 +0300

    Add project files.

---
+++ At_Yarısı/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace At_Yarısı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(304, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-9, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(703, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-9, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(703, 23);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(665, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 319);
+            this.label3.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::At_Yarısı.Properties.Resources.at2_hareketli_resim_02521;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 298);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(78, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::At_Yarısı.Properties.Resources.att3;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 179);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(78, 54);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::At_Yarısı.Properties.Resources.at2_hareketli_resim_0252;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Brown;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(301, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 27);
+            this.label4.TabIndex = 7;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Olive;
+            this.label5.Location = new System.Drawing.Point(-9, -5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(703, 56);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Olive;
+            this.label6.Location = new System.Drawing.Point(0, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(703, 56);
+            this.label6.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(695, 418);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "At Yarışı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ At_Yarısı/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At_Yarısı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rdm = new Random();
+        int zaman=0;
+        int boy;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            label4.Text = zaman.ToString();
+            zaman++;
+            pictureBox1.Left = pictureBox1.Left + rdm.Next(4,10);
+            pictureBox1.Width = pictureBox1.Width + rdm.Next(0, 2);
+
+            pictureBox2.Left = pictureBox2.Left + rdm.Next(4, 10);
+            pictureBox2.Width = pictureBox2.Width + rdm.Next(0, 2);
+
+            pictureBox3.Left = pictureBox3.Left + rdm.Next(4, 10);
+            pictureBox3.Width = pictureBox3.Width + rdm.Next(0, 2);
+
+            if (pictureBox1.Left+pictureBox1.Width > label3.Left)
+            { 
+                timer1.Enabled = false; MessageBox.Show("Yarışı birinci at kazandı");
+                pictureBox1.Left = 0;
+                pictureBox2.Left = 0;
+                pictureBox3.Left = 0;
+
+                pictureBox1.Width = boy;
+                pictureBox2.Width = boy;
+                pictureBox3.Width = boy;
+                zaman = 0;
+                label4.Text = "0";
+            }
+
+
+            if (pictureBox2.Left+pictureBox2.Width > label3.Left) 
+            { 
+                timer1.Enabled = false; MessageBox.Show("Yarışı ikinci at kazandı");
+                pictureBox1.Left = 0;
+                pictureBox2.Left = 0;
+                pictureBox3.Left = 0;
+
+                pictureBox1.Width = boy;
+                pictureBox2.Width = boy;
+                pictureBox3.Width = boy;
+                zaman = 0;
+                label4.Text = "0";
+            }
+
+
+            if (pictureBox3.Left+pictureBox3.Width > label3.Left) 
+            { 
+                timer1.Enabled = false; MessageBox.Show("Yarışı üçüncü at kazandı");
+                pictureBox1.Left = 0;
+                pictureBox2.Left = 0;
+                pictureBox3.Left = 0;
+
+                pictureBox1.Width = boy;
+                pictureBox2.Width = boy;
+                pictureBox3.Width = boy;
+                zaman = 0;
+                label4.Text = "0";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boy = pictureBox1.Width;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
